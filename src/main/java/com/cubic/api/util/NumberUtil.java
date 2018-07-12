a26a46c284845c54252d136bee74215497bd7ae9
liusibo <liusibo@DESKTOP-EIDQTAU>
2018-07-12 17:33:55 +0800

    About #2

--- src/main/java/com/cubic/api/util/NumberUtil.java
+++ src/main/java/com/cubic/api/util/NumberUtil.java
@@ -2,30 +2,15 @@ package com.cubic.api.util;
 
 /**
  * 房源、客源、用户编号生成工具
- *
+ * type:H房源,G客源,VK用户
  * @author fei.yu
  * @date 2018/07/12
  */
 public class NumberUtil {
-	public static String geoHouseEquipmentNo(long equipmentNo){
+	public static String geoHouseEquipmentNo(String type,long equipmentNo){
         String newEquipmentNo = "000000001";
-        long newEquipment = equipmentNo + 1;
-        newEquipmentNo = String.format("H" + "%09d", newEquipment);
+        long newEquipment = equipmentNo;
+        newEquipmentNo = String.format(type + "%09d", newEquipment);
         return newEquipmentNo;
     }
-	
-	public static String geoGuestEquipmentNo(long equipmentNo){
-        String newEquipmentNo = "000000001";
-        long newEquipment = equipmentNo + 1;
-        newEquipmentNo = String.format("G" + "%09d", newEquipment);
-        return newEquipmentNo;
-    }
-	
-	public static String geoUserEquipmentNo(long equipmentNo){
-        String newEquipmentNo = "000000001";
-        long newEquipment = equipmentNo + 1;
-        newEquipmentNo = String.format("VK" + "%09d", newEquipment);
-        return newEquipmentNo;
-    }
-
 }
