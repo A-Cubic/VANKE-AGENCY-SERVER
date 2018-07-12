da6e2d79da164fe46492df7717b1bd85b72b403a
liusibo <liusibo@DESKTOP-EIDQTAU>
2018-07-12 17:28:43 +0800

    About #2

--- src/main/java/com/cubic/api/util/NumberUtil.java
+++ src/main/java/com/cubic/api/util/NumberUtil.java
@@ -7,33 +7,24 @@ package com.cubic.api.util;
  * @date 2018/07/12
  */
 public class NumberUtil {
-	public static String geoHouseEquipmentNo(String equipmentNo){
+	public static String geoHouseEquipmentNo(long equipmentNo){
         String newEquipmentNo = "000000001";
-
-        if(equipmentNo != null && !equipmentNo.isEmpty()){
-            int newEquipment = Integer.parseInt(equipmentNo) + 1;
-            newEquipmentNo = String.format("H" + "%09d", newEquipment);
-        }
+        long newEquipment = equipmentNo + 1;
+        newEquipmentNo = String.format("H" + "%09d", newEquipment);
         return newEquipmentNo;
     }
 	
-	public static String geoGuestEquipmentNo(String equipmentNo){
+	public static String geoGuestEquipmentNo(long equipmentNo){
         String newEquipmentNo = "000000001";
-
-        if(equipmentNo != null && !equipmentNo.isEmpty()){
-            int newEquipment = Integer.parseInt(equipmentNo) + 1;
-            newEquipmentNo = String.format("G" + "%09d", newEquipment);
-        }
+        long newEquipment = equipmentNo + 1;
+        newEquipmentNo = String.format("G" + "%09d", newEquipment);
         return newEquipmentNo;
     }
 	
-	public static String geoUserEquipmentNo(String equipmentNo){
+	public static String geoUserEquipmentNo(long equipmentNo){
         String newEquipmentNo = "000000001";
-
-        if(equipmentNo != null && !equipmentNo.isEmpty()){
-            int newEquipment = Integer.parseInt(equipmentNo) + 1;
-            newEquipmentNo = String.format("VK" + "%09d", newEquipment);
-        }
+        long newEquipment = equipmentNo + 1;
+        newEquipmentNo = String.format("VK" + "%09d", newEquipment);
         return newEquipmentNo;
     }
 
