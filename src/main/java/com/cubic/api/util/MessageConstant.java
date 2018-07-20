d2f21734688229cb9d37125bd6f3b7ec6e2f523a
legendary <330120128@qq.com>
2018-07-20 14:48:09 +0800

    About #2

---
+++ src/main/java/com/cubic/api/util/MessageConstant.java
@@ -0,0 +1,10 @@
+package com.cubic.api.util;
+
+public class MessageConstant {
+	public static final String MESSAGE_HOUSE_SPECIAL = "提交了特殊房源申请";
+	public static final String MESSAGE_HOUSE_GOOD = "提交了优质房源申请";
+	public static final String MESSAGE_HOUSE_INVALID = "提交了无效房源申请";
+	public static final String MESSAGE_HOUSE_REALIMG = "提交了实勘图申请";
+	public static final String MESSAGE_GUEST_INVALID = "提交了无效客源申请";
+	
+}
